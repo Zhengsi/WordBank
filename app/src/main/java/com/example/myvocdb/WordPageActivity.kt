8e5c3d3b2bf068a410319c639c611bc733cda96b
Zhengsi <zxc160430@utdallas.edu>
2019-11-02 16:20:04 -0500

    added the audio recording module

---
+++ app/src/main/java/com/example/myvocdb/WordPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myvocdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WordPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word_page)
+    }
+}
